047d31d17a60b221c6c3288745c4f67c23f1a123
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2012-09-14 09:56:21 -0300

    [Estado inicial del proyecto] -Proyecto en blanco

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HolidayReminder::Application.config.secret_token = '4fd22e0aa816eff9a9a5c9d1f89ade0801f4f55e5999f8316da09129fc9c07d1495b8f0cf6a9c5d4af44863ae7c818e95fad2a91a96313d8e8a8536b4756a6ca'
