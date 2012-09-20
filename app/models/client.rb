c6860fe3c47b022f25b379fdfdef2c1279cfad13
Sergio Sebastian Arriagada <sarriagada87@gmail.com>
2012-09-20 10:55:09 -0300

    [Se valido cliente nombre y mail unicos y se modifico CSS tabla] -Se modifico el modelo client para agregar la validacion de name y mail uniqueness -Se modifico el CSS de la tabla de clientes para que admita como alto max 400px

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,6 +3,7 @@ class Client < ActiveRecord::Base
   attr_accessible :mail, :name, :pais, :user_id
 
   validates :name, :mail, :pais, :presence => true
+  validates :name, :mail, :uniqueness => true
 
   has_many :holidays
 
